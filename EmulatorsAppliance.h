580d1c4bdd35ea307807a742dd0a609d0bafc0c6
bgan1982 <bgan1982@10d5c73a-0ecf-11de-9ca8-93eb7b2a972e>
2009-07-10 16:15:52 +0000

    keywords?

--- EmulatorsAppliance.h
+++ EmulatorsAppliance.h
@@ -4,6 +4,11 @@
 //
 //  Created by bgan1982@mac.com (Ben) on 6/14/08.
 //
+//  $Date$
+//  $Rev$
+//  $Author$
+//  $HeadURL$
+//  $ID$
 
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
