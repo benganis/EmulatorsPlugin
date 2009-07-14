761ff66e3b7ca1313117dfffee0b60d7e8337e4f
bgan1982 <bgan1982@10d5c73a-0ecf-11de-9ca8-93eb7b2a972e>
2009-07-14 03:57:46 +0000

    Added "type" plist-tag for launching scripts/tools Fixed memory leak for allocation of menu controllers from appliance.

--- EmulatorsAppliance.h
+++ EmulatorsAppliance.h
@@ -1,6 +1,6 @@
 //
 //  EmulatorsAppliance.h
-//  EmulatorsPlugIn 2.0
+//  EmulatorsPlugIn 2.1
 //
 //  Created by bgan1982@mac.com (Ben) on 6/14/08.
 //
@@ -12,9 +12,11 @@
 #import <Foundation/Foundation.h>
 #import <Cocoa/Cocoa.h>
 #import <BackRow/BackRow.h>
+#import "EmulatorsApplianceMenuController.h"
 
 @interface EmulatorsAppliance : BRBaseAppliance
 {
+	EmulatorsApplianceMenuController *menuController;
 }
 
 @end
